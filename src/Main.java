8b819e72916f6953f58f7d18dabf2d4417cdd63a
Ghost5454 <adam1petchey@gmail.com>
2022-10-11 17:37:50 +0100

    Created presets

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String output;
+        Mode beginner = new Mode(9, 9, 10);
+        Mode intermediat = new Mode(16, 16, 40);
+        Mode expert = new Mode(30, 16, 99);
+
+        System.out.println("What difficulty do you want to play:\n((B)eginner, (I)ntermediat, (E)xpert or (C)ustom");
+
+        //TODO: input parsing
+        //TODO: custom mode
 
         Grid newGame = new Grid(10,8,13);
         output = newGame.RenderGrid();
---
+++ src/Mode.java
@@ -0,0 +1,14 @@
+public class Mode {
+
+    int width;
+    int height;
+    int mines;
+
+    Mode(int myWidth, int myHeight, int myMines)
+    {
+        this.width = myWidth;
+        this.height = myHeight;
+        this.mines = myMines;
+    }
+
+}
