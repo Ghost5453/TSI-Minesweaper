a31c7162675b829bbcc2f0e7b736be915cd872c2
Ghost5454 <adam1petchey@gmail.com>
2022-10-12 11:16:13 +0100

    User can choos there difulcty

--- src/Main.java
+++ src/Main.java
@@ -5,37 +5,62 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String output, inputString;
-        String[] test;
-        int conuter;
+        String output, inputString, lowerString;
+        String[] parcedInput;
+        Grid newGame;
+        char action;
+        int counter, height, width, mines;
         Mode beginner = new Mode(9, 9, 10);
-        Mode intermediat = new Mode(16, 16, 40);
+        Mode intermediate = new Mode(16, 16, 40);
         Mode expert = new Mode(30, 16, 99);
+        Mode current;
 
-        System.out.println("What difficulty do you want to play:\n((B)eginner, (I)ntermediat, (E)xpert or (C)ustom");
-
-        //TODO: input parsing
-        //TODO: custom mode
-
-        Grid newGame = new Grid(10,8,13);
-        output = newGame.RenderGrid();
-        System.out.println(output);
+        System.out.println("What difficulty do you want to play:\n((B)eginner, (I)ntermediate, (E)xpert or (C)ustom");
 
         inputString = input.nextLine();
+        parcedInput = InputParsing(inputString);
 
-        test = InputParsing(inputString);
+        lowerString = parcedInput[0].toLowerCase();
+        action = lowerString.charAt(0);
 
-        conuter = 0;
+        if (action == 'b')
+        {
+            current = beginner;
+        } else if(action == 'i')
+        {
+            current = intermediate;
+        } else if(action == 'e')
+        {
+            current = expert;
+        } else if(action == 'c')
+        {
+            System.out.println("What format do you want to use\n (type in the form: \"height\" \"width\" \"mines\")");
+            inputString = input.nextLine();
+            parcedInput = InputParsing(inputString);
+
+            try {
+                height = Integer.valueOf(parcedInput[0]).intValue();
+                width = Integer.valueOf(parcedInput[1]).intValue();
+                mines = Integer.valueOf(parcedInput[2]).intValue();
+
+                current = new Mode(height, width, mines);
+
+            } catch (Exception fail)
+            {
+                System.out.println("Invalid input using intermediate");
+                current = intermediate;
+            }
 
 
-        for (String mySt:test) {
-
-            output = test[conuter];
-
-            System.out.println(output);
-            conuter++;
-
+        } else
+        {
+            System.out.println("Invalid input using Intermediate");
+            current = intermediate;
         }
+
+        newGame = new Grid(current.height, current.width, current.mines);
+        output = newGame.RenderGrid();
+        System.out.println(output);
 
         input.nextLine();
 
